95792600b7fede926d491dce0c69c9def7c72de8
Debayan Basu <debayanbasu11@gmail.com>
2020-10-23 19:25:15 +0530

    committing initial project files

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{  
+
+    public GameObject[] platformPrefabs;
+
+
+    public int numberOfPlatforms = 200;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+  
+        for(int i=0; i<numberOfPlatforms; i++){
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Debug.Log(Random.Range(0, platformPrefabs.Length));
+            Instantiate(platformPrefabs[0], spawnPosition, Quaternion.identity);
+            if(i%4 ==0)
+                Instantiate(platformPrefabs[1], spawnPosition * Random.Range(0, platformPrefabs.Length), Quaternion.identity);
+            if(i%30 == 0)
+                Instantiate(platformPrefabs[2], spawnPosition * Random.Range(0, platformPrefabs.Length), Quaternion.identity);
+        }   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    Rigidbody2D rb;
+    float movement = 0f;
+    BoxCollider2D myFeet;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    void Update(){
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+        FlipSprite();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(myFeet.IsTouchingLayers(LayerMask.GetMask("Platform Breaker"))){
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void FlipSprite(){
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed){
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+    }
+}
